793cc506e816839fb0ab9643f621f0b2faa82ac3
Nick Alexander <alexander50@live.com>
2019-10-07 20:55:27 -0400

    Initial Commit

---
+++ Goodnight/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Goodnight
+//
+//  Created by Nick Alexander on 10/7/19.
+//  Copyright © 2019 Nick Alexander. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
