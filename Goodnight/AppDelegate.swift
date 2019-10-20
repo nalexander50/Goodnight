8b5693fd801d54e06c335b25860fc4b450c0b072
Nick Alexander <alexander50@live.com>
2019-10-20 02:30:56 -0400

    Trivial proof of concept

--- Goodnight/AppDelegate.swift
+++ Goodnight/AppDelegate.swift
@@ -8,32 +8,202 @@
 
 import Cocoa
 import SwiftUI
+import Combine
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
     // MARK: - Properties
-    
-    private var statusBarItem: NSStatusItem!
-    private var popoverManager: GNMenuPopoverManager!
+
+    private var statusItemMenuManager: StatusItemMenuManager!
+    private var subscription: AnyCancellable!
     
     // MARK: - NSApplicationDelegate
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        self.statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
-        self.statusBarItem.button?.title = "💤"
-        self.statusBarItem.button?.toolTip = "Sleeping Soon"
+        let subject = PassthroughSubject<GNUserInterfaceEvent, Never>()
+        self.statusItemMenuManager = StatusItemMenuManager(subject: subject)
+        self.statusItemMenuManager.buildMenu()
+                
+        let responder = GNUserInterfaceResponder(forSubject: subject)
         
-        self.popoverManager = GNMenuPopoverManager(anchoredTo: self.statusBarItem.button!, withContent: MenuBarPopoverView())
-        self.statusBarItem.button?.action = #selector(self.popoverManager.toggle)
-        
-        let menuBuilder = GNMenuBuilder(popoverManager: self.popoverManager)
-        self.statusBarItem.menu = menuBuilder.buildMenu()
+        self.subscription = subject.sink { event in
+            print(event)
+        }
     }
     
     func applicationWillResignActive(_ notification: Notification) {
-        self.popoverManager.close()
+        
+    }
+    
+    deinit {
+        self.subscription?.cancel()
     }
 
 }
 
+enum GNUserInterfaceEvent {
+    case statusItemClicked
+    case minutesItemClicked(minutes: Int)
+    
+    #if DEBUG
+    case secondsItemClicked(seconds: Int)
+    #endif
+}
+
+protocol GNUserInterfaceResponding {
+        
+    func selected(minutes: Int)
+    func selected(hours: Int)
+    func selected(date: Date)
+    func selected(batteryAbove percentage: Int)
+    func selected(runningApp: NSRunningApplication)
+    func selectedAbout()
+    func selectedPreferences()
+    
+    #if DEBUG
+    func debugSelected(seconds: Int)
+    #endif
+}
+
+class GNUserInterfaceResponder: GNUserInterfaceResponding {
+    
+    private var subscription: AnyCancellable? = nil
+    
+    init(forSubject subject: PassthroughSubject<GNUserInterfaceEvent, Never>) {
+        self.subscription = subject.sink { event in
+            switch event {
+                
+            case .statusItemClicked:
+                return
+            case .minutesItemClicked(let minutes):
+                self.selected(minutes: minutes)
+            #if DEBUG
+            case .secondsItemClicked(let seconds):
+                self.debugSelected(seconds: seconds)
+            #endif
+            @unknown default:
+                return
+            }
+        }
+    }
+    
+    deinit {
+        self.subscription?.cancel()
+    }
+    
+    func selected(minutes: Int) {
+        print("\(minutes) Minutes")
+    }
+    
+    func selected(hours: Int) {
+        print("\(hours) Hours")
+    }
+    
+    func selected(date: Date) {
+        print("Until \(date.description)")
+        
+    }
+    
+    func selected(batteryAbove percentage: Int) {
+        print("\(percentage)%")
+    }
+    
+    func selected(runningApp: NSRunningApplication) {
+        print("While \(runningApp.localizedName ?? "?")")
+    }
+    
+    func selectedAbout() {
+        print("About")
+    }
+    
+    func selectedPreferences() {
+        print("Preferences")
+    }
+    
+    #if DEBUG
+    func debugSelected(seconds: Int) {
+        print("\(seconds) Seconds")
+    }
+    #endif
+    
+}
+
+class StatusItemMenuManager {
+    
+    private let subject: PassthroughSubject<GNUserInterfaceEvent, Never>
+    private let statusItem: NSStatusItem
+    
+    init(subject: PassthroughSubject<GNUserInterfaceEvent, Never>) {
+        self.subject = subject
+        self.statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        self.statusItem.button?.title = "💤"
+        self.statusItem.button?.target = self
+        self.statusItem.button?.action = #selector(self.buttonAction)
+    }
+    
+    func buildMenu() {
+        self.statusItem.button?.target = nil
+        self.statusItem.button?.action = nil
+        
+        let topLevelMenu = NSMenu()
+        topLevelMenu.addItem(NSMenuItem.withBoldTitle(title: "Create Sleep Timer:", action: nil, keyEquivalent: ""))
+        #if DEBUG
+        topLevelMenu.addItem(self.buildSecondsMenuItem())
+        #endif
+        topLevelMenu.addItem(self.buildMinutesMenuItem())
+        
+        self.statusItem.menu = topLevelMenu
+    }
+    
+    func removeMenu() {
+        self.statusItem.menu = nil
+        self.statusItem.button?.target = self
+        self.statusItem.button?.action = #selector(self.buttonAction)
+    }
+    
+    @objc private func buttonAction() {
+        self.subject.send(.statusItemClicked)
+    }
+    
+    #if DEBUG
+    private func buildSecondsMenuItem() -> NSMenuItem {
+        let menuItem = GNActionableMenuItem(title: "Seconds", keyEquivalent: "") {
+            self.subject.send(.secondsItemClicked(seconds: 10))
+        }
+        menuItem.indentationLevel = 1
+        return menuItem
+    }
+    #endif
+    
+    private func buildMinutesMenuItem() -> NSMenuItem {
+        let menuItem = NSMenuItem(title: "Minutes", action: nil, keyEquivalent: "")
+        menuItem.indentationLevel = 1
+        menuItem.submenu = NSMenu()
+        
+        for minutes in ((1..<60).filter { $0 % 5 == 0 }) {
+            let item = GNActionableMenuItem(title: "\(minutes) Minutes", keyEquivalent: "") {
+                self.subject.send(.minutesItemClicked(minutes: minutes))
+            }
+            
+            menuItem.submenu!.addItem(item)
+            
+            if minutes % 10 == 0 {
+                item.keyEquivalent = "\(minutes / 10)"
+                item.keyEquivalentModifierMask = [.command]
+            } else if minutes > 5 && minutes % 5 == 0 {
+                item.keyEquivalent = "\(minutes / 10)"
+                item.keyEquivalentModifierMask = [.command, .option]
+            } else {
+                item.keyEquivalent = "0"
+            }
+            
+            if (minutes - 5) % 10 == 0 {
+                menuItem.submenu!.addItem(NSMenuItem.separator())
+            }
+        }
+        
+        return menuItem
+    }
+    
+}
--- Goodnight/GNSleepScheduler.swift
+++ Goodnight/GNSleepScheduler.swift
@@ -1,181 +1,181 @@
+////
+////  SleepManager.swift
+////  Goodnight
+////
+////  Created by Nick Alexander on 10/7/19.
+////  Copyright © 2019 Nick Alexander. All rights reserved.
+////
 //
-//  SleepManager.swift
-//  Goodnight
+//import Foundation
 //
-//  Created by Nick Alexander on 10/7/19.
-//  Copyright © 2019 Nick Alexander. All rights reserved.
-//
-
-import Foundation
-
-/// Responsible for scheduling future sleep events based on specified conditions
-class GNSleepScheduler {
-    
-    // MARK: - Singleton
-    
-    /// The current sleep scheduler.
-    static let current: GNSleepScheduler = GNSleepScheduler()
-    
-    // MARK: - Properties
-    
-    private var internalTimer: Timer?
-    private var sleepTimer: GNSleepTimer?
-    
-    /**
-     Returns `true` if there is a scheduled sleep event
-     */
-    var isSleepScheduled: Bool {
-        get {
-            return self.internalTimer != nil
-        }
-    }
-    
-    /// Returns a `string` indicating when the current device will sleep according to the active sleep timer.
-    var getScheduledSleepStatus: String? {
-        get {
-            guard let internalTimer = self.internalTimer else {
-                return nil
-            }
-            
-            guard let sleepTimer = self.sleepTimer else {
-                return nil
-            }
-            
-            switch sleepTimer {
-            
-            case .afterMinutes(_):
-                return self.formatTimeInterval(abs(Date().timeIntervalSince(internalTimer.fireDate)))
-            case .afterHours(_):
-                return self.formatTimeInterval(abs(Date().timeIntervalSince(internalTimer.fireDate)))
-            case .until(_):
-                return self.formatTimeInterval(abs(Date().timeIntervalSince(internalTimer.fireDate)))
-            case .whileBatteryAbove(let percentage):
-                return "Until battery level drops to \(percentage)%."
-            case .whileAppIsRunning(let app):
-                return "While \(app.localizedName ?? "Unknown Process") is running."
-            }
-        }
-    }
-    
-    // MARK: - Initializers
-    
-    /// Creates a new sleep scheduler with no scheduled events. For internal use only.
-    private init() {
-        self.internalTimer = nil
-        self.sleepTimer = nil
-    }
-    
-    // MARK: - Public Methods
-    
-    /**
-     Schedules the device to go to sleep after the specified sleep timer conditions are met. Before the device sleeps, the scheduler is returned to a default state.
-     
-     - Parameter sleepTimer: The sleep timer specifying under what conditions the device should go to sleep.
-     - Remark: Note: the device will enter sleep mode after the sleep timer resolves, and the scheduler will be returned to a default state.
-     */
-    func scheduleSleep(afterTimer sleepTimer: GNSleepTimer) {
-        self.sleepTimer = sleepTimer
-        self.internalTimer = self.createTimer(forSleepTimer: sleepTimer)
-        RunLoop.current.add(self.internalTimer!, forMode: .common)
-    }
-    
-    /**
-     Cancels the currently scheduled sleep and returns the scheduler to a default state.
-     */
-    func cancelScheduledSleep() {
-        self.internalTimer?.invalidate()
-        self.internalTimer = nil
-        self.sleepTimer = nil
-    }
-    
-    // MARK: - Create Timer Methods
-    
-    /**
-     Creates a timer that fires when the conditions specified by the sleep timer are met. When the timer fires, the device sleeps and the scheduler is returned to a default state. if the sleep timer specifies invalid conditions, returns `nil`.
-     
-     - Parameter sleepTimer: The sleep timer specifying under what conditions the device should go to sleep.
-     - Returns: A timer that fires when the sleep timer conditions are met. If the sleep timer specifies invalid conditions, returns `nil`.
-     */
-    private func createTimer(forSleepTimer sleepTimer: GNSleepTimer) -> Timer? {
-        switch sleepTimer {
-        
-        case .afterMinutes(let minutes):
-            if let fireDate = Calendar.current.date(byAdding: DateComponents(minute: minutes), to: Date()) {
-                return Timer(fire: fireDate, interval: 0, repeats: false, block: { timer in
-                    self.cancelScheduledSleep()
-                    self.sleepNow()
-                })
-            }
-        case .afterHours(let hours):
-            if let fireDate = Calendar.current.date(byAdding: DateComponents(hour: hours), to: Date()) {
-                return Timer(fire: fireDate, interval: 0, repeats: false, block: { timer in
-                    self.cancelScheduledSleep()
-                    self.sleepNow()
-                })
-            }
-        case .until(let date):
-            return Timer(fire: date, interval: 0, repeats: false) { timer in
-                self.cancelScheduledSleep()
-                self.sleepNow()
-            }
-            // TODO make polling intervale configurable?
-        case .whileBatteryAbove(let percentage):
-            return Timer(timeInterval: 60, repeats: true) { timer in
-                if let currentPercentage = self.getBatteryPercentage() {
-                    if currentPercentage < percentage {
-                        self.cancelScheduledSleep()
-                        self.sleepNow()
-                    }
-                }
-            }
-            // TODO make polling intervale configurable?
-        case .whileAppIsRunning(let application):
-            return Timer(timeInterval: 60, repeats: true) { timer in
-                if application.isTerminated {
-                    self.cancelScheduledSleep()
-                    self.sleepNow()
-                }
-            }
-        }
-        
-        return nil
-    }
-    
-    // MARK: - PMSet Interface Methods
-    
-    /**
-     Sends the device to sleep using the `pmset` utility.
-     */
-    private func sleepNow() {
-        let interface = PMSetInterface()
-        interface.sleepNow()
-    }
-    
-    /**
-     Retrieves the device's current battery percentage using the `pmset` utility. If the battery information cannot be determined, returns `nil`.
-     
-     - Returns: The device's current battery percentage otherwise `nil`.
-     */
-    private func getBatteryPercentage() -> Int? {
-        let interface = PMSetInterface()
-        return interface.getBatteryStatus()?.batteryPercentage
-    }
-    
-    // MARK: - Utility Methods
-    
-    /**
-     Formats a time interval into a human readable string.
-     
-     - Parameter timeInterval: The time interval to format.
-     - Returns: A human readable string representing the time interval.
-     */
-    private func formatTimeInterval(_ timeInterval: TimeInterval) -> String? {
-        let formatter = DateComponentsFormatter()
-        formatter.allowedUnits = [.day, .hour, .minute, .second]
-        formatter.allowsFractionalUnits = false
-        formatter.maximumUnitCount = 4
-        formatter.unitsStyle = .full
-        return formatter.string(from: timeInterval)
-    }
-    
-}
+///// Responsible for scheduling future sleep events based on specified conditions
+//class GNSleepScheduler {
+//    
+//    // MARK: - Singleton
+//    
+//    /// The current sleep scheduler.
+//    static let current: GNSleepScheduler = GNSleepScheduler()
+//    
+//    // MARK: - Properties
+//    
+//    private var internalTimer: Timer?
+//    private var sleepTimer: GNSleepTimer?
+//    
+//    /**
+//     Returns `true` if there is a scheduled sleep event
+//     */
+//    var isSleepScheduled: Bool {
+//        get {
+//            return self.internalTimer != nil
+//        }
+//    }
+//    
+//    /// Returns a `string` indicating when the current device will sleep according to the active sleep timer.
+//    var getScheduledSleepStatus: String? {
+//        get {
+//            guard let internalTimer = self.internalTimer else {
+//                return nil
+//            }
+//            
+//            guard let sleepTimer = self.sleepTimer else {
+//                return nil
+//            }
+//            
+//            switch sleepTimer {
+//            
+//            case .afterMinutes(_):
+//                return self.formatTimeInterval(abs(Date().timeIntervalSince(internalTimer.fireDate)))
+//            case .afterHours(_):
+//                return self.formatTimeInterval(abs(Date().timeIntervalSince(internalTimer.fireDate)))
+//            case .until(_):
+//                return self.formatTimeInterval(abs(Date().timeIntervalSince(internalTimer.fireDate)))
+//            case .whileBatteryAbove(let percentage):
+//                return "Until battery level drops to \(percentage)%."
+//            case .whileAppIsRunning(let app):
+//                return "While \(app.localizedName ?? "Unknown Process") is running."
+//            }
+//        }
+//    }
+//    
+//    // MARK: - Initializers
+//    
+//    /// Creates a new sleep scheduler with no scheduled events. For internal use only.
+//    private init() {
+//        self.internalTimer = nil
+//        self.sleepTimer = nil
+//    }
+//    
+//    // MARK: - Public Methods
+//    
+//    /**
+//     Schedules the device to go to sleep after the specified sleep timer conditions are met. Before the device sleeps, the scheduler is returned to a default state.
+//     
+//     - Parameter sleepTimer: The sleep timer specifying under what conditions the device should go to sleep.
+//     - Remark: Note: the device will enter sleep mode after the sleep timer resolves, and the scheduler will be returned to a default state.
+//     */
+//    func scheduleSleep(afterTimer sleepTimer: GNSleepTimer) {
+//        self.sleepTimer = sleepTimer
+//        self.internalTimer = self.createTimer(forSleepTimer: sleepTimer)
+//        RunLoop.current.add(self.internalTimer!, forMode: .common)
+//    }
+//    
+//    /**
+//     Cancels the currently scheduled sleep and returns the scheduler to a default state.
+//     */
+//    func cancelScheduledSleep() {
+//        self.internalTimer?.invalidate()
+//        self.internalTimer = nil
+//        self.sleepTimer = nil
+//    }
+//    
+//    // MARK: - Create Timer Methods
+//    
+//    /**
+//     Creates a timer that fires when the conditions specified by the sleep timer are met. When the timer fires, the device sleeps and the scheduler is returned to a default state. if the sleep timer specifies invalid conditions, returns `nil`.
+//     
+//     - Parameter sleepTimer: The sleep timer specifying under what conditions the device should go to sleep.
+//     - Returns: A timer that fires when the sleep timer conditions are met. If the sleep timer specifies invalid conditions, returns `nil`.
+//     */
+//    private func createTimer(forSleepTimer sleepTimer: GNSleepTimer) -> Timer? {
+//        switch sleepTimer {
+//        
+//        case .afterMinutes(let minutes):
+//            if let fireDate = Calendar.current.date(byAdding: DateComponents(minute: minutes), to: Date()) {
+//                return Timer(fire: fireDate, interval: 0, repeats: false, block: { timer in
+//                    self.cancelScheduledSleep()
+//                    self.sleepNow()
+//                })
+//            }
+//        case .afterHours(let hours):
+//            if let fireDate = Calendar.current.date(byAdding: DateComponents(hour: hours), to: Date()) {
+//                return Timer(fire: fireDate, interval: 0, repeats: false, block: { timer in
+//                    self.cancelScheduledSleep()
+//                    self.sleepNow()
+//                })
+//            }
+//        case .until(let date):
+//            return Timer(fire: date, interval: 0, repeats: false) { timer in
+//                self.cancelScheduledSleep()
+//                self.sleepNow()
+//            }
+//            // TODO make polling intervale configurable?
+//        case .whileBatteryAbove(let percentage):
+//            return Timer(timeInterval: 60, repeats: true) { timer in
+//                if let currentPercentage = self.getBatteryPercentage() {
+//                    if currentPercentage < percentage {
+//                        self.cancelScheduledSleep()
+//                        self.sleepNow()
+//                    }
+//                }
+//            }
+//            // TODO make polling intervale configurable?
+//        case .whileAppIsRunning(let application):
+//            return Timer(timeInterval: 60, repeats: true) { timer in
+//                if application.isTerminated {
+//                    self.cancelScheduledSleep()
+//                    self.sleepNow()
+//                }
+//            }
+//        }
+//        
+//        return nil
+//    }
+//    
+//    // MARK: - PMSet Interface Methods
+//    
+//    /**
+//     Sends the device to sleep using the `pmset` utility.
+//     */
+//    private func sleepNow() {
+//        let interface = PMSetInterface()
+//        interface.sleepNow()
+//    }
+//    
+//    /**
+//     Retrieves the device's current battery percentage using the `pmset` utility. If the battery information cannot be determined, returns `nil`.
+//     
+//     - Returns: The device's current battery percentage otherwise `nil`.
+//     */
+//    private func getBatteryPercentage() -> Int? {
+//        let interface = PMSetInterface()
+//        return interface.getBatteryStatus()?.batteryPercentage
+//    }
+//    
+//    // MARK: - Utility Methods
+//    
+//    /**
+//     Formats a time interval into a human readable string.
+//     
+//     - Parameter timeInterval: The time interval to format.
+//     - Returns: A human readable string representing the time interval.
+//     */
+//    private func formatTimeInterval(_ timeInterval: TimeInterval) -> String? {
+//        let formatter = DateComponentsFormatter()
+//        formatter.allowedUnits = [.day, .hour, .minute, .second]
+//        formatter.allowsFractionalUnits = false
+//        formatter.maximumUnitCount = 4
+//        formatter.unitsStyle = .full
+//        return formatter.string(from: timeInterval)
+//    }
+//    
+//}
